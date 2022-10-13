cff60894c99ec9eb57f4dfa95bb67c6cc96ff2d8
Lauty821 <LautarodeFCBarcelona@outlook.com>
2022-10-13 12:39:10 -0300

    Commit N°1

---
+++ Quienes somos.php
@@ -0,0 +1,9 @@
+<?php include("template/cabecera.php"); ?>
+
+    <div class="jumbotron">
+        <h1 class="display-3">¿Quienes somos?</h1>
+        <p class="lead">Somos una comunidad que comparte mangas y comics a través de esta plataforma.</p>
+        <hr class="my-2">
+    </div>
+    
+<?php include("template/pie.php"); ?>
---
+++ template/cabecera.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astro Boys</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+
+                <a class="nav-link" href="#">Astro Boys</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+
+            <li class="nav-item">
+                <a class="nav-link" href="Mangas y Cómics.php">Mangas y Comics</a>
+
+            <li class="nav-item">
+                <a class="nav-link" href="Quienes somos.php">¿Quienes somos?</a>
+
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
